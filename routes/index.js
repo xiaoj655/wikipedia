17493a7787f020d10eb18cb83d4b7eced8eaf0d2
brook <123456@gmai.com>
2023-12-31 22:39:21 +0800

    123

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express')
 var router = express.Router()
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
-const proxyMiddleware = createProxyMiddleware({
-  target: 'https://zh.wikipedia.org/',
-  changeOrigin: true,
-})
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', proxyMiddleware)
+router.get('/', (req, res) => {
+  console.log('abc', req.url)
+})
 
 module.exports = router
